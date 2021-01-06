78f0e677ba7ec57a9a523ed52cb1f21d3e0c84f7
mke2111 <roymke134@gmail.com>
2021-01-06 08:28:37 +0300

    update gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 12 }, uniqueness: true
-  validates :fullName, presence: true, length: { maximum: 15 }
+  validates :fullname, presence: true, length: { maximum: 15 }
 
   has_many :talks, foreign_key: 'author_id', dependent: :destroy
 
