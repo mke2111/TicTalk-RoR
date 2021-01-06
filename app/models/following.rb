fac6814cefad39078ca2ff5c4f6ab01ac79a3c03
mke2111 <roymke134@gmail.com>
2021-01-06 08:32:27 +0300

    add following model associations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,12 @@
 class Following < ApplicationRecord
+  belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
+  belongs_to :followed, class_name: 'User', foreign_key: 'followed_id'
+
+  validate :cannot_follow_self
+
+  private
+
+  def cannot_follow_self
+    errors.add(:followed, 'You cannot follow yourself.') if follower == followed
+  end
 end
