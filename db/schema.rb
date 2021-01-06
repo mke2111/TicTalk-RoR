467abdfe289e0732554194afc412e922744077a0
mke2111 <roymke134@gmail.com>
2021-01-06 08:17:45 +0300

    add like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_051119) do
+ActiveRecord::Schema.define(version: 2021_01_06_051519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2021_01_06_051119) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "talk_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["talk_id"], name: "index_likes_on_talk_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "talks", force: :cascade do |t|
@@ -38,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_01_06_051119) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "likes", "talks"
+  add_foreign_key "likes", "users"
 end
