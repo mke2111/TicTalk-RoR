7921df36a3af3f83c15a95660f4cb206d40d188e
mke2111 <roymke134@gmail.com>
2021-01-06 08:30:51 +0300

    add talk model associations

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,2 +1,6 @@
 class Talk < ApplicationRecord
+  validates :content, presence: true, length: { minimum: 3, maximum: 200 }
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes
 end
