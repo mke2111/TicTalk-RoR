827c676272a82881cc49de2f1449a26d10b817e7
mke2111 <roymke134@gmail.com>
2021-01-06 08:42:09 +0300

    add talks controller methods

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,74 +1,40 @@
 class TalksController < ApplicationController
-  before_action :set_talk, only: [:show, :edit, :update, :destroy]
+  before_action :set_talk, only: %i[show edit update destroy]
 
-  # GET /talks
-  # GET /talks.json
   def index
-    @talks = Talk.all
-  end
-
-  # GET /talks/1
-  # GET /talks/1.json
-  def show
-  end
-
-  # GET /talks/new
-  def new
     @talk = Talk.new
+    @talks = Talk.all.includes(:author).order('created_at DESC')
+    @users = User.all - helpers.current_user.following - [helpers.current_user]
   end
 
-  # GET /talks/1/edit
-  def edit
-  end
-
-  # POST /talks
-  # POST /talks.json
   def create
-    @talk = Talk.new(talk_params)
+    @talk = Talk.new(author_id: session[:user_id], content: talk_params[:content])
 
     respond_to do |format|
       if @talk.save
-        format.html { redirect_to @talk, notice: 'Talk was successfully created.' }
+        format.html { redirect_to talks_path, notice: 'talk was successfully created.' }
         format.json { render :show, status: :created, location: @talk }
       else
         format.html { render :new }
-        format.json { render json: @talk.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /talks/1
-  # PATCH/PUT /talks/1.json
-  def update
-    respond_to do |format|
-      if @talk.update(talk_params)
-        format.html { redirect_to @talk, notice: 'Talk was successfully updated.' }
-        format.json { render :show, status: :ok, location: @talk }
-      else
-        format.html { render :edit }
-        format.json { render json: @talk.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /talks/1
-  # DELETE /talks/1.json
   def destroy
     @talk.destroy
     respond_to do |format|
-      format.html { redirect_to talks_url, notice: 'Talk was successfully destroyed.' }
+      format.html { redirect_to talks_url, notice: 'talk was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_talk
-      @talk = Talk.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def talk_params
-      params.require(:talk).permit(:author_id, :content)
-    end
+  def set_talk
+    @talk = Talk.find(params[:id])
+  end
+
+  def talk_params
+    params.require(:talk).permit(:content)
+  end
 end
